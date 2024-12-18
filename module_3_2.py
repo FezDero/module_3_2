c028597c92f411a20932023f5e3b6046dfb70c56
FezDero <fezdero@gmail.com>
2024-12-18 22:07:33 +0500

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,18 @@
+ending = ('.com', '.ru', '.net')
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    if '@' not in recipient or not recipient.endswith(ending) or '@' not in sender or not sender.endswith(ending):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+    if sender == recipient:
+        print ('Нельзя отправить письмо самому себе!')
+        return
+    if sender == 'university.help@gmail.com':
+        print (f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email("Это сообщение дял проверки связи", 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
